30129ed7e9eb0637f59ed560bff9c1778b455513
cmsbuild <>
2006-07-26 11:49:16 +0000

    This commit was manufactured by cvs2git to create tag 'OnlineDB- SiStripESSources-CMSSW_1_0_5'.
    Sprout from master 2006-07-26 11:48:57 UTC Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch> 'trivial change'
    Delete:
        test/BuildFile
        test/PopulateDb.cff
        test/PopulateDb.cfg
        test/PopulateDb.cfi
        test/SealModule.cc
        test/SiStripPopulateConfigDb.cc
        test/SiStripPopulateConfigDb.h

--- test/SealModule.cc
+++
@@ -1,6 +0,0 @@
-#include "PluginManager/ModuleDef.h"
-DEFINE_SEAL_MODULE();
-
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "OnlineDB/SiStripESSources/test/SiStripPopulateConfigDb.h"
-DEFINE_ANOTHER_FWK_MODULE(SiStripPopulateConfigDb)
